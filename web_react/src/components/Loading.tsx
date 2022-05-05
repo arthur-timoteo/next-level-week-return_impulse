0d19e69c9224e941a5e66aba7114552a162dbb41
Arthur <arthur.timoteo@gmail.com>
2022-05-05 20:25:11 -0300

    Stage 2 - Screenshot button

---
+++ web_react/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { Circle } from "phosphor-react";
+
+export function Loading(){
+    return(
+        <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
+            <Circle weight="bold" className="w-4 h-4 animate-spin" />
+        </div>
+    )
+}
--- web_react/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web_react/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -8,6 +10,8 @@ interface FeedbackContentStepProps {
 }
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps){
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+    
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return(
@@ -36,6 +40,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 />
 
                 <footer className="flex gap-2 mt-2">
+                    <ScreenshotButton 
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
                     <button
                         type="submit"
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
