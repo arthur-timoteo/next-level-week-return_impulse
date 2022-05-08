8ce2e56f359dc5d2f447f8f57e2f210305bb8fee
Arthur <arthur.timoteo@gmail.com>
2022-05-07 21:11:56 -0300

    Stage 3 - Jest configured and unit tests created

--- server_nodejs/src/routes.ts
+++ server_nodejs/src/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { PrismaFeedbackRepositopry } from './repositories/prisma/prisma-feedbacks-repository';
-import { SubmitFeedbackUseCase } from './repositories/use-cases/submit-feedback-use-case';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
