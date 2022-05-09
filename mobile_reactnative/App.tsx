fe43e974cfea44513d44614a81e18687e88f22b0
Arthur <arthur.timoteo@gmail.com>
2022-05-08 23:12:16 -0300

    Stage 4 - Custom fonts added

--- mobile_reactnative/App.tsx
+++ mobile_reactnative/App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
 import { Widget } from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={{
       flex: 1,
@@ -18,6 +29,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
+      <Text style={{ color: theme.colors.brand }}>adas dasd ad a d adas d ads dsadasdas as dsadasdas da as dasdasdasd a d ada sd </Text>
     </View>
   );
 }
