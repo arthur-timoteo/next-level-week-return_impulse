37c2ccd831b0d2e034ec733dbce7712ff9ed444f
Arthur <arthur.timoteo@gmail.com>
2022-05-09 22:27:43 -0300

    Stage 4 - Gesture with BottomSheet

--- mobile_reactnative/App.tsx
+++ mobile_reactnative/App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
@@ -6,7 +8,7 @@ import * as Font from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -48,13 +50,13 @@ export default function App() {
       onLayout={onLayoutRootView}
     >
       
-      <Widget />
-
       <StatusBar 
         style="light"
         backgroundColor="transparent"
         translucent
       />
+
+        <Widget />
     </View>
   );
 }
